d8b90d07c63ca721a0c17f81337d1380781bf13b
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-11 20:14:50 +0300

    EPIS_KPC_Follow_Up_Project.py

--- Mid_Project_Visualization_Streamlit.py
+++ Mid_Project_Visualization_Streamlit.py
@@ -7,7 +7,7 @@ import plotly.express as px  #7
 from plotly.subplots import make_subplots  #8
 import plotly.figure_factory as ff #21
 import datetime
-#from PIL import Image
+from PIL import Image
 
 Excel_Name="Client_EPIS_Daily_Progress.xlsx"
 df1=pd.read_csv(Excel_Name)
@@ -32,18 +32,16 @@ df1.SPENT_DAYS=df1.SPENT_DAYS.astype(str)+"/"+df1.JOB_DAYS.astype(str)
 st.markdown(" <center>  <h1> Used Car Dataset Analysis </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
 
-im = Image.open("audi-logo-2016.png")
+im = Image.open("EPIS.png")
 image = np.array(im)
 st.image(image)
 
 
-st.markdown(" <center>  <h1> A) Data Absolute Values </h1> </font> </center> </h1> ",
+st.markdown(" <center>  <h1> A) #########</h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
 
 
-data['KPL']=data['MPG'].apply(lambda x: x*0.425144)
 
-data['KILOMETERSAGE']=data['MILEAGE'].apply(lambda x: x*19.10)
 
 
 
@@ -54,34 +52,14 @@ st.markdown(" <right>  <h1> (I) The Car of the lowest selected feature</h1> </fo
 
 RB2=st.radio("Select The Desired Feature: ",('TAX','PRICE','MILEAGE'))
 if RB2=='TAX':
-    O2=data[data['TAX']==data['TAX'].min()] #### The Model That has the lowest Mile Age
-    st.write(O2)
-
-elif RB2 == 'PRICE':
-   O2=data[data['PRICE']==data['PRICE'].min()]   #### The Model That has the highest Mile Age
-   st.write(O2)
-   
-elif RB2 == 'MILEAGE':
-   O2=data[data['MILEAGE']==data['MILEAGE'].min()] #### The Model That has the lowest Mile Age
-   st.write(O2)
-
+            st.write("F")
 
 st.markdown(" <right> <h1> (II) The Car of the highest selected feature</h1> </font> </right> </h1> ",
             unsafe_allow_html=True)
 
 RB1=st.radio("Select one of The Desired Feature: ",('TAX','PRICE','MILEAGE'))
 if RB1=='TAX':
-    O1=data[data['TAX']==data['TAX'].max()]   #### The Model That has the highest Mile Age
-    st.write(O1)
-
-elif RB1 == 'PRICE':
-   O1=data[data['PRICE']==data['PRICE'].max()]   #### The Model That has the highest Mile Age
-   st.write(O1)
-elif RB1 == 'MILEAGE':
-   O1=data[data['MILEAGE']==data['MILEAGE'].max()]   #### The Model That has the highest Mile Age
-   st.write(O1)
-
-
+    st.write("S")
 
 st.markdown(" <center>  <h1> B) Data Average Values </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
@@ -92,15 +70,10 @@ Tog1 = st.selectbox("Desired Model Selection ",(' A1', ' A6', ' A4', ' A3' ,' Q3
 
 Y=st.slider('Select the year you are interested in',1997,2020)
 
-O3=data[(data['MODEL']==Tog1) & (data['YEAR']==Y) ].mean(axis=0)['TAX']
-O4=data[(data['MODEL']==Tog1)& (data['YEAR']==Y)].mean(axis=0)['PRICE']
-O5=data[(data['MODEL']==Tog1)& (data['YEAR']==Y)].mean(axis=0)['MILEAGE']
-O6=data[(data['MODEL']==Tog1)& (data['YEAR']==Y)].mean(axis=0)['ENGINESIZE']
+#O3=data[(data['MODEL']==Tog1) & (data['YEAR']==Y) ].mean(axis=0)['TAX']
+O3=4
 
 st.write(f'The Average Taxes for the model {Tog1} ',O3,'Euro')
-st.write(f'The Average Price for the model {Tog1} ',O4,'Euro')
-st.write(f'The Average Mileage for the model {Tog1} ',O5,'Miles')
-st.write(f'The Average ENGINESIZE for the model {Tog1} ',O6)
 
 
 # streamlit run "C:\\Users\\hp\\Desktop\\Data Science\\Mid-Project\\Data_Visulaization_Project_Files\\Mid_Project_Visualization_Streamlit.py"
