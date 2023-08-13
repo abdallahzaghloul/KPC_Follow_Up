e4346ee70a91733bca097b557973e7ce96e8cadd
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-13 12:19:54 +0300

    Update EPIS_KPC_Follow_Up.py

--- EPIS_KPC_Follow_Up.py
+++ EPIS_KPC_Follow_Up.py
@@ -79,7 +79,7 @@ for i in range(0,len(L)):
             if RB1==L[i]:
                         Critical = df2[df2['RIG_NO.']==L[i]]
                         Critical.drop(['POINT','FINAL_STATUS'],axis=1,inplace=True)
-                        #Critical=Critical.set_index('ZONE')
+                        Critical=Critical.set_index('ZONE')
                         st.dataframe(Critical.style.highlight_max(axis=0))
 
 
