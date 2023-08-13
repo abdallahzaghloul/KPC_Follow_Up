fd48e3934b9e3573008f0bad819b889c686180b3
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-13 18:49:45 +0300

    Update EPIS_KPC_Follow_Up.py

--- EPIS_KPC_Follow_Up.py
+++ EPIS_KPC_Follow_Up.py
@@ -83,10 +83,12 @@ for i in range(0,len(L)):
                         Critical.drop(['POINT','RIG_NO.'],axis=1,inplace=True)
                         Critical=Critical.set_index('ZONE')
                         st.dataframe(Critical,use_container_width=True)
-                        if st.button("Open"):
-                                    Critical = df2[df2['RIG_NO.']==L[i]]
-                                    Critical_Open=Critical[Critical['FINAL_STATUS']=="Open"]
-                                    st.dataframe(Critical,use_container_width=True)
+            if RB1==L[i] and st.button("Open"):
+                        Critical = df2[df2['RIG_NO.']==L[i]]
+                        Critical.drop(['POINT','RIG_NO.'],axis=1,inplace=True)
+                        Critical=Critical.set_index('ZONE')
+                        Critical_Open=Critical[Critical['FINAL_STATUS']=="Open"]
+                        st.dataframe(Critical_Open,use_container_width=True)
                                     
 
 
