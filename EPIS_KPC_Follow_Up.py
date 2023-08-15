a383acbab5b9c8fffbd4fc85f56a920948034aa8
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-15 16:25:17 +0300

    Update EPIS_KPC_Follow_Up.py

--- EPIS_KPC_Follow_Up.py
+++ EPIS_KPC_Follow_Up.py
@@ -82,10 +82,12 @@ RB1=st.radio("Select an Active Rig: ",Rigs)
 
 Team=df1
 Team.reset_index(inplace=True)
-
+S=Team[Team['RIG_NO.']=="EDC-40"]['TEAM_NO.']
+S=tuple(S)
+S[0]
 for i in range(0,len(Rigs)):
             if RB1==Rigs[i]:
-                        st.write(f"Critical Points of Rig {Rigs[i]} {(Team[Team['RIG_NO.']==Rigs[i]]['TEAM_NO.'][0])} ")
+                        st.write(f"Critical Points of Rig {Rigs[i]} {(S[0])} ")
                         Critical = df3[df3['RIG_NO.']==Rigs[i]]
                         Critical.drop(['RIG_NO.','LOCATION','REF.','PRIORITY'],axis=1,inplace=True)
                         Critical=Critical.set_index('NO.')
