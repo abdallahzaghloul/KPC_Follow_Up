06b819359637fb112b7641188406c70f8abf1d9c
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-18 23:13:14 +0300

    Update All_Critical_Points.py

--- pages/All_Critical_Points.py
+++ pages/All_Critical_Points.py
@@ -105,12 +105,9 @@ if (Phases_Slider!="All"):
     Day_Count=float(Day_Count[0])
     Years=math.floor(Day_Count/365)
     Months=math.floor(12*(np. remainder(Day_Count/365,1))) 
-    Days=round (30*(np. remainder(12*(np. remainder(Day_Count/365,1)),1)),0)
-  elif  Phases_Slider not in Rig_Phase:
-    st.write("There were no critical points @ {Phases_Slider}")
-  
-  V=len(list(df4[(df4["RIG_NO."]==RB1)&(df4['PHASE']==Phases_Slider)]['PHASE']))
-  st.write(f"Total Points of Rig {RB1} @ {Phases_Slider}  = {V} Points")                  
+    Days=round (30*(np. remainder(12*(np. remainder(Day_Count/365,1)),1)),0) 
+    V=len(list(df4[(df4["RIG_NO."]==RB1)&(df4['PHASE']==Phases_Slider)]['PHASE']))
+    st.write(f"Total Points of Rig {RB1} @ {Phases_Slider}  = {V} Points")                  
 elif  (Phases_Slider=="All"):
     Day_Count=list(df6[(df6["RIG_NO."]==RB1)&(df6['PHASE']==Phases_Slider)]['DAYS_COUNT'])
     V=len(list(df4[(df4["RIG_NO."]==RB1)]['PHASE']))
