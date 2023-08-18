38c46f269ee00263f81c884aed66725e0faefde0
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-18 16:33:24 +0300

    Update Daily_Follow_Up.py

--- Daily_Follow_Up.py
+++ Daily_Follow_Up.py
@@ -104,7 +104,7 @@ for i in range(0,len(Rigs)):
             if RB1==Rigs[i]:
                         S=Team[Team['RIG_NO.']==Rigs[i]]['TEAM_NO.']
                         S=tuple(S)
-                        st.write(f"Critical Points of Rig {Rigs[i]} ({S[0]}) ")
+                        st.write(f"Critical Points of Rig {Rigs[i]} ({S}) ")
                         Critical = df3[df3['RIG_NO.']==Rigs[i]]
                         Critical.drop(['RIG_NO.','LOCATION','REF.','PRIORITY'],axis=1,inplace=True)
                         Critical=Critical.set_index('NO.')
