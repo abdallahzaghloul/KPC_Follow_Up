7f0118bc0c593fefd151e633305e5d7df5cabc43
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-18 21:35:54 +0300

    Update All_Critical_Points.py

--- pages/All_Critical_Points.py
+++ pages/All_Critical_Points.py
@@ -64,7 +64,7 @@ df6['DAYS_COUNT'] = df6.TODAY_DATE-df6.DATE
 #df6=df6.set_index('RIG_NO.')
 df6.drop('TODAY_DATE',axis=1,inplace=True)
 df6['DATE']=df6['DATE'].dt.strftime('%d-%m-%Y')
-
+df6['DAYS_COUNT']=df6['DAYS_COUNT'].str.split(' ').str[0]
 
 
 
