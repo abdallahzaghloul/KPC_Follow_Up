718592f33642861761222f6a59c5e862d3183891
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-20 20:59:15 +0300

    Update All_Critical_Points.py

--- pages/All_Critical_Points.py
+++ pages/All_Critical_Points.py
@@ -15,13 +15,21 @@ df1 = pd.read_excel(File,'Audit_Teams_Follow_Up')
 df1.columns  = [i.replace(' ','_') for i in df1.columns]
 df1.columns  = [i.upper() for i in df1.columns]
 df1.dropna(axis=0, inplace=True)
-
+df1['RIG_NO.']  = [i.replace(' ','') for i in df1['RIG_NO.']]
+df1['RIG_NO.']  = [i.upper() for i in df1['RIG_NO.']]
+df1['JOB_TYPE']  = [i.replace(' ','') for i in df1['JOB_TYPE']]
+df1['JOB_TYPE']  = [i.upper() for i in df1['JOB_TYPE']]
 
 ######################## df4 #############################################################
 df4 = pd.read_excel(File,'All_Critical_Points')
 df4.columns  = [i.replace(' ','_') for i in df4.columns]
 df4.columns  = [i.upper() for i in df4.columns]
 df4.dropna(axis=0, inplace=True)
+
+df4['RIG_NO.']  = [i.replace(' ','') for i in df4['RIG_NO.']]
+df4['RIG_NO.']  = [i.upper() for i in df4['RIG_NO.']]
+df4['PHASE']  = [i.replace(' ','') for i in df4['PHASE']]
+df4['PHASE']  = [i.upper() for i in df4['PHASE']]
 
 df4['FINAL_STATUS']=df4['FINAL_\nSTATUS']
 df4.drop(['PRIORITY','REF.','FINAL_\nSTATUS'],axis=1, inplace=True)
@@ -51,6 +59,10 @@ PU_Rigs=tuple(PU_Rigs)
 df5 = pd.read_excel(File,'phases')
 df5.columns  = [i.replace(' ','_') for i in df5.columns]
 df5.columns  = [i.upper() for i in df5.columns]
+df5.dropna(axis=0, inplace=True)
+
+df5['PHASES']  = [i.replace(' ','') for i in df5['PHASES']]
+df5['PHASES']  = [i.upper() for i in df5['PHASES']]
 
 ######################## df6 #############################################################
 
@@ -58,6 +70,11 @@ df6 = pd.read_excel(File,'Phase_Dates')
 df6.columns  = [i.replace(' ','_') for i in df6.columns]
 df6.columns  = [i.upper() for i in df6.columns]
 df6.dropna(axis=0, inplace=True)
+df6['RIG_NO.']  = [i.replace(' ','') for i in df6['RIG_NO.']]
+df6['RIG_NO.']  = [i.upper() for i in df6['RIG_NO.']]
+df6['PHASE']  = [i.replace(' ','') for i in df6['PHASE']]
+df6['PHASE']  = [i.upper() for i in df6['PHASE']]
+
 
 #df6['DATE']=df6['DATE'].astype(str)
 #df6['DATE']=df6['DATE'].str.split(' ').str[0]
@@ -89,7 +106,7 @@ RB1=st.radio("Select DRLG Rig: ",DRLG_Rigs)
 Phases_Slider = st.select_slider('Select DRLG Phase', options=DRLG_Phases)
 
 for i in range (0,len(DRLG_Rigs)):
-            if (RB1==DRLG_Rigs[i]) and (Phases_Slider=="All"):
+            if (RB1==DRLG_Rigs[i]) and (Phases_Slider=="ALL"):
                         All_Critical=df4[(df4['RIG_NO.']==DRLG_Rigs[i])]
                         All_Critical.drop(['LOCATION','RIG_NO.','RIG_TYPE'],axis=1, inplace=True)
                         All_Critical=All_Critical.set_index("NO.")
@@ -97,14 +114,14 @@ for i in range (0,len(DRLG_Rigs)):
                       
                        
 
-            if (RB1==DRLG_Rigs[i]) and (Phases_Slider!="All"):                       
+            if (RB1==DRLG_Rigs[i]) and (Phases_Slider!="ALL"):                       
                         All_Critical=df4[(df4['RIG_NO.']==DRLG_Rigs[i])&(df4['PHASE']==Phases_Slider)]
                         All_Critical.drop(['LOCATION','RIG_NO.','RIG_TYPE'],axis=1, inplace=True)
                         All_Critical=All_Critical.set_index("NO.")
                         T1=st.dataframe(All_Critical,use_container_width=True)
 
 Rig_Phase_1=list(df4[(df4["RIG_NO."]==RB1)]['PHASE'])
-if (Phases_Slider!="All"):
+if (Phases_Slider!="ALL"):
   if Phases_Slider in Rig_Phase_1:
     st.write(f"Critical Points of Rig {RB1} ")
     Day_Count=list(df6[(df6["RIG_NO."]==RB1)&(df6['PHASE']==Phases_Slider)]['DAYS_COUNT'])
@@ -115,7 +132,7 @@ if (Phases_Slider!="All"):
     V=len(list(df4[(df4["RIG_NO."]==RB1)&(df4['PHASE']==Phases_Slider)]['PHASE']))
     st.write(f"Total Points of Rig {RB1} @ {Phases_Slider}  = {V} Points")
     st.write(f"These points were open since {Years} Years {Months} Months {Days} Days")    
-elif  (Phases_Slider=="All"):
+elif  (Phases_Slider=="ALL"):
     st.write(f"Critical Points of Rig {RB1} ")
     V=len(list(df4[(df4["RIG_NO."]==RB1)]['PHASE']))
     st.write(f"Total Points of Rig {RB1} = {V} Points")
@@ -134,13 +151,13 @@ RB2=st.radio("SelectWO Rig: ",WO_Rigs)
 Phases_Sliderr = st.select_slider('Select Phase WO', options=WO_Phases)
 
 for ii in range (0,len(WO_Rigs)):
-            if (RB2==WO_Rigs[ii]) and (Phases_Sliderr=="All"):
+            if (RB2==WO_Rigs[ii]) and (Phases_Sliderr=="ALL"):
                         st.write(f"Critical Points of Rig {WO_Rigs[ii]} ")
                         All_Criticall=df4[(df4['RIG_NO.']==WO_Rigs[ii])]
                         All_Criticall.drop(['LOCATION','RIG_NO.','RIG_TYPE'],axis=1, inplace=True)
                         All_Criticall=All_Criticall.set_index("NO.")
                         T1=st.dataframe(All_Criticall,use_container_width=True)    
-            if (RB2==WO_Rigs[ii]) and (Phases_Sliderr!="All"):
+            if (RB2==WO_Rigs[ii]) and (Phases_Sliderr!="ALL"):
                         st.write(f"Critical Points of Rig {WO_Rigs[ii]} ")
                         All_Criticall=df4[(df4['RIG_NO.']==WO_Rigs[ii])&(df4['PHASE']==Phases_Sliderr)]
                         All_Criticall.drop(['LOCATION','RIG_NO.','RIG_TYPE'],axis=1, inplace=True)
@@ -148,7 +165,7 @@ for ii in range (0,len(WO_Rigs)):
                         T1=st.dataframe(All_Criticall,use_container_width=True)                                    
 
 Rig_Phase_2=list(df4[(df4["RIG_NO."]==RB2)]['PHASE'])
-if (Phases_Sliderr!="All"):
+if (Phases_Sliderr!="ALL"):
   if Phases_Sliderr in Rig_Phase_2:
     st.write(f"Critical Points of Rig {RB2} ")
     Day_Count=list(df6[(df6["RIG_NO."]==RB2)&(df6['PHASE']==Phases_Sliderr)]['DAYS_COUNT'])
@@ -164,7 +181,6 @@ if (Phases_Sliderr!="All"):
     
 elif  (Phases_Sliderr=="All"):
     st.write(f"Critical Points of Rig {RB2} ")
-    Day_Count=list(df6[(df6["RIG_NO."]==RB2)&(df6['PHASE']==Phases_Sliderr)]['DAYS_COUNT'])
     V=len(list(df4[(df4["RIG_NO."]==RB2)]['PHASE']))
     st.write(f"Total Points of Rig {RB2} = {V} Points")
 
@@ -180,13 +196,13 @@ RB3=st.radio("Select PU : ",PU_Rigs)
 Phases_Sliderrr = st.select_slider('Select Phase PU', options=PU_Phases)
 
 for iii in range (0,len(PU_Rigs)):
-            if (RB3==PU_Rigs[iii]) and (Phases_Sliderrr=="All"):
+            if (RB3==PU_Rigs[iii]) and (Phases_Sliderrr=="ALL"):
                         st.write(f"Critical Points of Rig {PU_Rigs[iii]} ")
                         All_Criticalll=df4[(df4['RIG_NO.']==PU_Rigs[iii])]
                         All_Criticalll.drop(['LOCATION','RIG_NO.','RIG_TYPE'],axis=1, inplace=True)
                         All_Criticalll=All_Criticalll.set_index("NO.")
                         T1=st.dataframe(All_Criticalll,use_container_width=True)    
-            if (RB3==PU_Rigs[iii]) and (Phases_Sliderrr!="All"):
+            if (RB3==PU_Rigs[iii]) and (Phases_Sliderrr!="ALL"):
                         st.write(f"Critical Points of Rig {PU_Rigs[iii]} ")
                         All_Criticalll=df4[(df4['RIG_NO.']==PU_Rigs[iii])&(df4['PHASE']==Phases_Sliderrr)]
                         All_Criticalll.drop(['LOCATION','RIG_NO.','RIG_TYPE'],axis=1, inplace=True)
@@ -195,7 +211,7 @@ for iii in range (0,len(PU_Rigs)):
 
 
 Rig_Phase_3=list(df4[(df4["RIG_NO."]==RB3)]['PHASE'])
-if (Phases_Sliderrr!="All"):
+if (Phases_Sliderrr!="ALL"):
   if Phases_Sliderrr in Rig_Phase_3:
     st.write(f"Critical Points of Rig {RB3} ")
     Day_Count=list(df6[(df6["RIG_NO."]==RB3)&(df6['PHASE']==Phases_Sliderrr)]['DAYS_COUNT'])
@@ -211,7 +227,6 @@ if (Phases_Sliderrr!="All"):
     
 elif  (Phases_Sliderr=="All"):
     st.write(f"Critical Points of Rig {RB3} ")
-    Day_Count=list(df6[(df6["RIG_NO."]==RB3)&(df6['PHASE']==Phases_Sliderrr)]['DAYS_COUNT'])
     V=len(list(df4[(df4["RIG_NO."]==RB3)]['PHASE']))
     st.write(f"Total Points of Rig {RB3} = {V} Points")
 
