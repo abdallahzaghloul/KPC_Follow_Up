5713b46f63d8c97c1c1bdd8f7459e5f17cb09af3
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-20 20:41:28 +0300

    Update Daily_Follow_Up.py

--- Daily_Follow_Up.py
+++ Daily_Follow_Up.py
@@ -45,6 +45,9 @@ df1['CRITICAL_CLOSURE_%']=df1['CRITICAL_CLOSURE_%']+"%"
 df2 = pd.read_excel(File,'Drops_Teams_Follow_Up')
 df2.columns  = [i.replace(' ','_') for i in df2.columns]
 df2.columns  = [i.upper() for i in df2.columns]
+df2['RIG_NO.']  = [i.replace(' ','') for i in df2['RIG_NO.']]
+df2['RIG_NO.']  = [i.upper() for i in df2['RIG_NO.']]
+
 df2.dropna (axis=0, inplace=True)
 df2['LAST_VISIT']=df2['LAST_VISIT'].astype(str)
 df2['LAST_VISIT']=df2['LAST_VISIT'].str.split(' ').str[0]
