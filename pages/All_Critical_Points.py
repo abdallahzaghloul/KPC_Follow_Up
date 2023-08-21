4289a49ae1ff064f78bdbb1605abc8b1566f3511
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-08-21 22:20:08 +0300

    Update All_Critical_Points.py

--- pages/All_Critical_Points.py
+++ pages/All_Critical_Points.py
@@ -135,9 +135,9 @@ if (Phases_Slider!="ALL"):
   elif Phases_Slider not in Rig_Phase_1:
     st.write(f"There were no registered points @ {Phases_Slider} ")            
 elif  (Phases_Slider=="ALL"):
-            if ["NOCRITICAL"] in  Rig_Phase_1:
+            if list(df4[(df4["RIG_NO."]==RB1)]['PHASE'])==["NOCRITICAL"]:
                         st.write(f"There were no registered points @ All ")   
-            elif ["NOCRITICAL"] not in  Rig_Phase_1:
+            elif list(df4[(df4["RIG_NO."]==RB1)]['PHASE'])!=["NOCRITICAL"] :
                          st.write(f"Critical Points of Rig {RB1} ")
                          V=len(list(df4[(df4["RIG_NO."]==RB1)]['PHASE']))
                          st.write(f"Total Points of Rig {RB1} = {V} Points")
